a0b279d96b1b32519d5c8b17e74aa3abdf4d1554
Alexander Schmitz <alexander.schmitz@tu-berlin.de>
2022-12-19 11:31:31 +0100

    debugging

--- html_report/list_branches.sh
+++ html_report/list_branches.sh
@@ -3,3 +3,9 @@
 # https://man7.org/linux/man-pages/man1/git-for-each-ref.1.html
 rm branches.txt -f
 git branch --format='%(refname:lstrip=3)' -r >> branches.txt
+$FILE=branches.txt
+if test -f "$FILE"; then
+  echo "$FILE exists."
+else
+  echo "$FILE does not exist"
+fi
