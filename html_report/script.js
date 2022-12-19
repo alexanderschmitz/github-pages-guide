72abe462a3d7aa44a5fed0b9593fb9b4c7c6fed3
Alexander Schmitz <alexander.schmitz@tu-berlin.de>
2022-12-19 11:04:02 +0100

    remove jquery

--- html_report/script.js
+++ html_report/script.js
@@ -5,21 +5,22 @@ function makeUL(array) {
 
     for (var i = 0; i < array.length; i++) {
         var item = document.createElement('li');
-        item.appendChild(document.createTextNode(array[i]));
+        item.appendChild(document.createTextNode(LINK_PREFIX.concat(array[i])));
         list.appendChild(item);
     }
 
     // Finally, return the constructed list:
     return list;
 }
-$(document).ready(function() {
+
+document.addEventListener("DOMContentLoaded", function (event) {
     fetch('branches.txt')
-        .then(response => response.text())
-        .then(text => text.split("\n"))
-        .then(array => array.filter(n => n))
-        .then(array => {
-            console.log(array);
-            return array;
-        })
-        .then(array => document.getElementById('foo').appendChild(makeUL(array)));
+    .then(response => response.text())
+    .then(text => text.split("\n"))
+    .then(array => array.filter(n => n))
+    .then(array => {
+        console.log(array);
+        return array;
+    })
+    .then(array => document.getElementById('foo').appendChild(makeUL(array)));
 })
