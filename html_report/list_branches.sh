3e7c550eb033f0b478c877a231a4e41ddbe6aa8f
Alexander Schmitz <alexander.schmitz@tu-berlin.de>
2022-12-19 10:40:04 +0100

    index page with remote repos
    remove jquery
    
    refactoring
    
    fix: remove old txt file
    
    debugging
    
    debugging
    
    debugging
    
    add favicon
    
    index page for reports
    
    formating
    
    refactor: store config data in config file
    
    fix: promise handling
    
    added css styilization
    
    refactor: writing branches.txt

---
+++ html_report/list_branches.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# https://man7.org/linux/man-pages/man1/git-for-each-ref.1.html
+git branch --format='%(refname:lstrip=3)' -r > html_report/branches.txt
+FILE=html_report/branches.txt
+if test -f "$FILE"; then
+  echo "$FILE exists."
+else
+  echo "$FILE does not exist"
+fi
