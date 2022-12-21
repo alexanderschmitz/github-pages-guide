0fc15abe328260ec58a8ccc4fbf88fd5e83b1e4d
Alexander Schmitz <alexander.schmitz@tu-berlin.de>
2022-12-21 10:59:01 +0100

    refactor: writing branches.txt

--- html_report/list_branches.sh
+++ html_report/list_branches.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # https://man7.org/linux/man-pages/man1/git-for-each-ref.1.html
-rm html_report/branches.txt -f
-git branch --format='%(refname:lstrip=3)' -r >> html_report/branches.txt
+git branch --format='%(refname:lstrip=3)' -r > html_report/branches.txt
 FILE=html_report/branches.txt
 if test -f "$FILE"; then
   echo "$FILE exists."
